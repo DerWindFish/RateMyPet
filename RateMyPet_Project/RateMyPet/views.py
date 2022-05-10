c4b26560260efe49fa0a2fe1fd2dcf13a842cdcf
Mark Ryan <markrryan99@gmail.com>
2022-05-10 09:13:24 -0700

    set up views

--- RateMyPet_Project/RateMyPet/views.py
+++ RateMyPet_Project/RateMyPet/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import UserSerializer, PetSerializer, UserRatingSerializer
+from .models import User, Pet, UserRating
 
-# Create your views here.
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PetList(generics.ListCreateAPIView):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+class PetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Pet.objects.all()
+    serializer_class = PetSerializer
+
+class UserRatingList(generics.ListCreateAPIView):
+    queryset = UserRating.objects.all()
+    serializer_class = UserRatingSerializer
+
+class UserRatingDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserRating.objects.all()
+    serializer_class = UserRatingSerializer
