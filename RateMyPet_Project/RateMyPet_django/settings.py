f0260e0e88fb7b443f1f3b438761fd8944a549d0
Mark Ryan <markrryan99@gmail.com>
2022-05-10 09:20:05 -0700

    installed rest framework

--- RateMyPet_Project/RateMyPet_django/settings.py
+++ RateMyPet_Project/RateMyPet_django/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'RateMyPet'
+    'RateMyPet',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
