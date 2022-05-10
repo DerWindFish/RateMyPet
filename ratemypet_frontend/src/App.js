1bc0217349621b90b276a6e2d729380e7f13e481
Mark Ryan <markrryan99@gmail.com>
2022-05-10 09:43:01 -0700

    edited appjs

--- ratemypet_frontend/src/App.js
+++ ratemypet_frontend/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import PrivateRoute from './services/PrivateRoute'
+import { AuthProvider } from './context/AuthContext'
+
+import HomePage from './pages/Homepage'
+import LoginPage from './pages/LoginPage'
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <AuthProvider>
+          <Nav />
+          <PrivateRoute components={HomePage} path='/' exact />
+          <Route component={LoginPage} path='/login' />
+        </AuthProvider>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
