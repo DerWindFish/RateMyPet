367f049726c778331ba51113aabf4d816e9402fd
Mark Ryan <markrryan99@gmail.com>
2022-05-12 06:53:47 -0700

    added links to nav

--- ratemypet_frontend/src/components/Nav.jsx
+++ ratemypet_frontend/src/components/Nav.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Nav = () => {
+const Nav = ({authenticated, user}) => {
+    let authOptions
+    if (user) {
+        authOptions = (
+            <nav className="navbar">
+                <h4>Welcome in, {user.username}!</h4>
+                <Link to='/homepage'>Home Page</Link>
+                <Link to='/user_options'>User</Link>
+                <Link to='/ratethepets'>The Pets</Link>
+            </nav>
+        )
+    }
 
+    const unAuthOptions = (
+        <nav className="navbar">
+            <Link to='/'>Home</Link>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+        </nav>
+    )
 
     return (
-        <Link to='/'>
-            <div className="logo-wrapper" alt='logo'>
-                <img className="logo" alt='ratemypetlogo' src='https://imgur.com/cUheGkw.png' />
-            </div>
-        </Link>
+        <header>
+            <Link to='/'>
+                <div className="logo-wrapper" alt='logo'>
+                    <img className="logo" alt='ratemypetlogo' src='https://imgur.com/cUheGkw.png' />
+                </div>
+            </Link>
+            {authenticated && user ? authOptions : unAuthOptions}
+        </header>
     )
 }
 
